693e6e5eba460f2ad738e31b006c444111e88588
Celso Alexandre <celsoalexandre@live.com>
2019-10-02 22:37:10 -0300

    Aula de Quarta-Feira - WebFrontend

---
+++ backend/src/server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const routes = require('./routes')
+const path = require('path')
+
+const app = express()
+
+mongoose.connect('mongodb+srv://test:123@test-api-qk3vk.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
+
+//req.params.nomedoParametro (Para parametro dentro da URL [com :], sem ?. Utiliza-se para put, get, delete)
+//req.query.nomeParametro (Parametro utilizando ?nomedoParametro=valor dentro da URL. Utiliza-se para filtros de get)
+//req.body = Para acessar o body da requisicao (post, put)
+
+app.use(cors())
+app.use(express.json())
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')))
+app.use(routes)
+
+app.listen(3333)
