a76046a51d2f5ad8097c5d6acb8009b43033b7c8
Celso Alexandre <celsoalexandre@live.com>
2019-10-02 22:46:31 -0300

    Small security change

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,10 +3,11 @@ const mongoose = require('mongoose')
 const cors = require('cors')
 const routes = require('./routes')
 const path = require('path')
+require('dotenv').config()
 
 const app = express()
 
-mongoose.connect('mongodb+srv://test:123@test-api-qk3vk.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
 
 //req.params.nomedoParametro (Para parametro dentro da URL [com :], sem ?. Utiliza-se para put, get, delete)
 //req.query.nomeParametro (Parametro utilizando ?nomedoParametro=valor dentro da URL. Utiliza-se para filtros de get)
